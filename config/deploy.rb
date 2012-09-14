1a39054722b09b7ef45887817ef75f0bef2c9936
Daya S <daya.naukri@gmail.com>
2012-09-14 18:28:55 +0545

    first commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,9 +18,9 @@ set :deploy_via, :remote_cache
 set :deploy_to, "/var/www/cms"
 set :repository_cache, "cached_copy"
 
-role :web, "rubynewauth.com"                          # Your HTTP server, Apache/etc
-role :app, "rubynewauth.com"                          # This may be the same as your `Web` server
-role :db,  "rubynewauth.com", :primary => true # This is where Rails migrations will run
+# role :web, "rubynewauth.com"                          # Your HTTP server, Apache/etc
+# role :app, "rubynewauth.com"                          # This may be the same as your `Web` server
+# role :db,  "rubynewauth.com", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
@@ -50,4 +50,11 @@ end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+end
+
+task :staging do
+  set :rails_env, 'staging'
+  role :app, "rubynewauth.com"
+  role :web, "rubynewauth.com"
+  role :db,  "rubynewauth.com", :primary => true
 end
